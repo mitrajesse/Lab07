370c346badd607b6fb49a9d899f8199c15c1f71b
mitrajesse <mitrajesse@gmail.com>
2024-10-10 12:14:28 -0400

    Initial Commit

---
+++ Lab07/Lab07App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab07App.swift
+//  Lab07
+//
+//  Created by Jesse Mitra on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
